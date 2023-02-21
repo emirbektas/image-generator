70bc3c888a524f3e8eb56bdc8afeca2a2ce6fa72
emirbkts <emirbktts@gmail.com>
2023-02-21 18:01:11 +0300

    api done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import "./App.css";
-
-// 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
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
 
 function App() {
-  return <div className="App"></div>;
+  const [prompt, setPrompt] = useState("0");
+  const [result, setResult] = useState("");
+
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "512x512",
+    });
+
+    setResult(res.data.data[0].url);
+  };
+  return (
+    <div className="App">
+      <h2>Generate an Image using Open AI API</h2>
+      <textarea
+        placeholder="let's generate an image..."
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <br />
+      <button onClick={generateImage}>Generate an Image</button>
+      <hr />
+      {result.length > 0 ? <img src={result} alt="ai img" /> : <p>No data!</p>}
+    </div>
+  );
 }
 
 export default App;
