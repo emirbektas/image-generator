bbe2ca7efc2c39f2e8c39a35bd9d8a9c9691ce3f
emirbkts <emirbktts@gmail.com>
2023-02-21 18:02:11 +0300

    space update

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
   return (
     <div className="App">
       <h2>Generate an Image using Open AI API</h2>
+      <h4>Use "-" instead of space button!</h4>
       <textarea
         placeholder="let's generate an image..."
         onChange={(e) => setPrompt(e.target.value)}
